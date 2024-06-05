cf5927540b5292f5efb3d65b660e5195d3d51290
Kody Baker <kody.bkr@gmail.com>
2024-06-05 19:16:17 -0400

    commit up until i needed to create another key; need a way to have a key stored in a public sp

--- main.tf
+++ main.tf
@@ -73,3 +73,39 @@ resource "aws_security_group" "public-sg" {
     cidr_blocks = ["0.0.0.0/0"]
   }
 }
+
+
+resource "aws_key_pair" "homelab-auth" {
+  key_name   = "kb-auth"
+  public_key = file("~/.ssh/id_ecdsa.pub")
+
+}
+
+
+resource "aws_instance" "dev-node" {
+  instance_type          = "t3-micro"
+  ami                    = data.aws_ami.server_ami.id
+  key_name               = aws_key_pair.homelab-auth.id
+  vpc_security_group_ids = [aws_security_group.public-sg.id]
+  subnet_id              = aws_subnet.public-subnet1.id
+  user_data              = file("userdata.tpl")
+
+  root_block_device {
+    volume_size = 10
+  }
+
+  tags = {
+    Name = "dev-node"
+  }
+
+  provisioner "local-exec" {
+    command = templatefile("linux-ssh-config.tpl", {
+      hostname     = self.public_ip
+      user         = "ubuntu"
+      identityFile = "~/.ssh/id_ecdsa"
+
+    })
+    interpreter = ["bash", "-c"]
+
+  }
+}
