60ca005ac74e47c91b0d4c8e9d67bd12e61552e2
Kody Baker <kody.bkr@gmail.com>
2024-06-05 18:33:25 -0400

    s

--- main.tf
+++ main.tf
@@ -9,17 +9,18 @@ resource "aws_vpc" "homelab-vpc" {
 
 }
 
-resource "aw_subnet" "public-subnet1" {
+resource "aws_subnet" "public-subnet1" {
   vpc_id                  = aws_vpc.homelab-vpc.id
   cidr_block              = "10.123.0.0/16"
   map_public_ip_on_launch = true
   availability_zone       = "us-west-2a"
 
 
-  tags {
+  tags = {
     Name = "public-subnet1"
   }
 }
+
 
 
 resource "aws_internet_gateway" "homelab-igw" {
@@ -45,10 +46,30 @@ resource "aws_route_table" "homelab-rt" {
 resource "aws_route" "default_route" {
   route_table_id         = aws_route_table.homelab-rt.id
   destination_cidr_block = "0.0.0.0/16"
-  gateway_id             = aws_interneterraform.homelab-rt.id
+  gateway_id             = aws_internet_gateway.homelab-igw.id
+}
 
-  tags = {
-    Name = "default-rt"
+resource "aws_route_table_association" "homelab-rta" {
+  route_table_id = aws_route_table.homelab-rt.id
+  gateway_id     = aws_internet_gateway.homelab-igw.id
+}
+
+resource "aws_security_group" "public-sg" {
+  name        = "dev_sg"
+  description = "dev security group"
+  vpc_id      = aws_vpc.homelab-vpc.id
+
+  ingress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["73.172.56.221/32"]
   }
 
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
 }
